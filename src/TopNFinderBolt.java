5ad28c1108acd6868cc59a463293af26630eacd6
martinaas <maregu@gmail.com>
2015-09-27 11:05:45 +0100

    Excercise 1

--- src/TopNFinderBolt.java
+++ src/TopNFinderBolt.java
@@ -6,51 +6,68 @@ import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Values;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * a bolt that finds the top n words.
  */
 public class TopNFinderBolt extends BaseBasicBolt {
-  private HashMap<String, Integer> currentTopWords = new HashMap<String, Integer>();
-  private int N;
+    private HashMap<String, Integer> currentTopWords = new HashMap<String, Integer>();
+    private int N;
 
-  private long intervalToReport = 20;
-  private long lastReportTime = System.currentTimeMillis();
+    private long intervalToReport = 20;
+    private long lastReportTime = System.currentTimeMillis();
 
-  public TopNFinderBolt(int N) {
-    this.N = N;
-  }
-
-  @Override
-  public void execute(Tuple tuple, BasicOutputCollector collector) {
-    if (currentTopWords.size() < N)
-        currentTopWords.put(tuple.getString(0), Integer.parseInt(tuple.getString(1)));
-
-    //reports the top N words periodically
-    if (System.currentTimeMillis() - lastReportTime >= intervalToReport) {
-      collector.emit(new Values(printMap()));
-      lastReportTime = System.currentTimeMillis();
+    public TopNFinderBolt(int N) {
+        this.N = N;
     }
-  }
 
-  @Override
-  public void declareOutputFields(OutputFieldsDeclarer declarer) {
+    @Override
+    public void execute(Tuple tuple, BasicOutputCollector collector) {
+        String word = tuple.getString(0);
+        Integer count = tuple.getInteger(1);
+        currentTopWords.put(word, count);
+        if (currentTopWords.size() > N) {
 
-     declarer.declare(new Fields("top-N"));
+            String keyWithLowestCount = null;
+            int min = 0;
+            for (Map.Entry entry : currentTopWords.entrySet()) {
+                int countValue = Integer.parseInt((String) entry.getValue());
+                if (min > countValue) {
+                    min = countValue;
+                    keyWithLowestCount = (String) entry.getKey();
+                }
+            }
 
-  }
-
-  public String printMap() {
-    StringBuilder stringBuilder = new StringBuilder();
-    stringBuilder.append("top-words = [ ");
-    for (String word : currentTopWords.keySet()) {
-      stringBuilder.append("(" + word + " , " + currentTopWords.get(word) + ") , ");
+            if (count > min && keyWithLowestCount != null) {
+                currentTopWords.remove(keyWithLowestCount);
+            }
+        }
+        //reports the top N words periodically
+        if (System.currentTimeMillis() - lastReportTime >= intervalToReport) {
+            collector.emit(new Values(printMap()));
+            lastReportTime = System.currentTimeMillis();
+        }
     }
-    int lastCommaIndex = stringBuilder.lastIndexOf(",");
-    stringBuilder.deleteCharAt(lastCommaIndex + 1);
-    stringBuilder.deleteCharAt(lastCommaIndex);
-    stringBuilder.append("]");
-    return stringBuilder.toString();
 
-  }
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+
+        declarer.declare(new Fields("top-N"));
+
+    }
+
+    public String printMap() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("top-words = [ ");
+        for (String word : currentTopWords.keySet()) {
+            stringBuilder.append("(" + word + " , " + currentTopWords.get(word) + ") , ");
+        }
+        int lastCommaIndex = stringBuilder.lastIndexOf(",");
+        stringBuilder.deleteCharAt(lastCommaIndex + 1);
+        stringBuilder.deleteCharAt(lastCommaIndex);
+        stringBuilder.append("]");
+        return stringBuilder.toString();
+
+    }
 }
