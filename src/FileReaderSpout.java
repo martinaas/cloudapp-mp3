b29ffdceefcbbb6bd1634c85c74c05718461e503
martinaas <maregu@gmail.com>
2015-09-27 09:39:46 +0100

    Excercise 1

--- src/FileReaderSpout.java
+++ src/FileReaderSpout.java
@@ -11,78 +11,85 @@ import backtype.storm.topology.IRichSpout;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Values;
+import backtype.storm.utils.Utils;
 
 public class FileReaderSpout implements IRichSpout {
-  private SpoutOutputCollector _collector;
-  private TopologyContext context;
+    private SpoutOutputCollector _collector;
+    private TopologyContext context;
+    private FileReader fileReader;
+    private BufferedReader bufferedReader;
 
 
-  @Override
-  public void open(Map conf, TopologyContext context,
-                   SpoutOutputCollector collector) {
+    @Override
+    public void open(Map conf, TopologyContext context,
+                     SpoutOutputCollector collector) {
 
-     /*
-    ----------------------TODO-----------------------
-    Task: initialize the file reader
+        String fileName = (String) conf.get("fileName");
+        if (null != fileName) {
+
+            try {
+                fileReader = new FileReader(fileName);
+                bufferedReader = new BufferedReader(fileReader);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        this.context = context;
+        this._collector = collector;
+    }
+
+    @Override
+    public void nextTuple() {
+
+        Utils.sleep(100);
+        try {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                _collector.emit(new Values(line));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Utils.sleep(100);
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+
+        declarer.declare(new Fields("word"));
+
+    }
+
+    @Override
+    public void close() {
+        try {
+            if (fileReader != null)
+                fileReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
-    ------------------------------------------------- */
+    @Override
+    public void activate() {
+    }
 
-    this.context = context;
-    this._collector = collector;
-  }
+    @Override
+    public void deactivate() {
+    }
 
-  @Override
-  public void nextTuple() {
+    @Override
+    public void ack(Object msgId) {
+    }
 
-     /*
-    ----------------------TODO-----------------------
-    Task:
-    1. read the next line and emit a tuple for it
-    2. don't forget to sleep when the file is entirely read to prevent a busy-loop
+    @Override
+    public void fail(Object msgId) {
+    }
 
-    ------------------------------------------------- */
-
-
-  }
-
-  @Override
-  public void declareOutputFields(OutputFieldsDeclarer declarer) {
-
-    declarer.declare(new Fields("word"));
-
-  }
-
-  @Override
-  public void close() {
-   /*
-    ----------------------TODO-----------------------
-    Task: close the file
-
-
-    ------------------------------------------------- */
-
-  }
-
-
-  @Override
-  public void activate() {
-  }
-
-  @Override
-  public void deactivate() {
-  }
-
-  @Override
-  public void ack(Object msgId) {
-  }
-
-  @Override
-  public void fail(Object msgId) {
-  }
-
-  @Override
-  public Map<String, Object> getComponentConfiguration() {
-    return null;
-  }
+    @Override
+    public Map<String, Object> getComponentConfiguration() {
+        return null;
+    }
 }
